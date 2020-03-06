033c04ea09136b29373dc0a606af837d55969f56
sztxr <sztxr@github.com>
2020-03-05 21:24:31 -0300

    design constrains

--- sketch.js
+++ sketch.js
@@ -1,21 +1,35 @@
+const CRYSTAL_SIZE = 500;
+const SIDES = 6;
+let PALETTE = [];
+
 function setup() {
-  createCanvas(500, 500, SVG);
+  createCanvas(530, 530, SVG);
+
+  PALETTE = [
+    color(255, 52, 154), // pink
+    color(4, 0, 152) // blue
+  ];
+
   noLoop();
   angleMode(DEGREES);
   rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
-  noStroke();
-  fill(0);
+  background(250);
+}
 
+function testLines() {
+  noFill(0);
   push();
   translate(width / 2, height / 2);
-  rotate(45);
-  rect(0, 0, 25, 25);
+  stroke(PALETTE[0]);
+  ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+  stroke(PALETTE[1]);
+  const angle = 360 / SIDES;
+  for (let i = 0; i < SIDES; i++) {
+    line(0, 0, 0, CRYSTAL_SIZE / 2);
+    rotate(angle);
+  }
   pop();
-
-  fill('red');
-  rect(0, 0, 25, 25);
 }
