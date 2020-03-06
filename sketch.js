fba90662a49d1bd839e957d1f7b94e009f06280a
sztxr <sztxr@github.com>
2020-03-05 21:26:27 -0300

    using random()

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
   PALETTE = [
     color(255, 52, 154), // pink
-    color(4, 0, 152) // blue
+    color(4, 0, 152), // blue
   ];
 
   noLoop();
@@ -17,17 +17,32 @@ function setup() {
 
 function draw() {
   background(250);
+  testLines();
 }
 
 function testLines() {
+  const randomSides = random(1);
+  let numberOfShapes;
+  if (randomSides > 0.5) {
+    numberOfShapes = SIDES;
+  } else {
+    numberOfShapes = SIDES * 2;
+  }
+  // console.log(randomSides, numberOfShapes);
+
+  const randomColour = floor(random(0, PALETTE.length));
+  const strokeColour = PALETTE[randomColour];
+  // console.log(randomColour);
+
   noFill(0);
   push();
   translate(width / 2, height / 2);
   stroke(PALETTE[0]);
   ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-  stroke(PALETTE[1]);
-  const angle = 360 / SIDES;
-  for (let i = 0; i < SIDES; i++) {
+
+  stroke(strokeColour);
+  const angle = 360 / numberOfShapes;
+  for (let i = 0; i < numberOfShapes; i++) {
     line(0, 0, 0, CRYSTAL_SIZE / 2);
     rotate(angle);
   }
