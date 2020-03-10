dfa21180c5ad0750414f3c37491a1f5c02d20dfa
sztxr <sztxr@github.com>
2020-03-10 18:54:06 -0300

    composition pt.3 refactor

--- Layer.js
+++ Layer.js
@@ -18,7 +18,7 @@ const circles = (state) => {
   state.position = (CRYSTAL_SIZE / 2) - (state.shapeSize / 2); // position = outer shape radius - inner shape radius
 
   return ({
-    name: 'circles',
+    name: 'Circles',
     render: () => {
       noFill();
       stroke(state.layerColour);
@@ -31,165 +31,165 @@ const circles = (state) => {
       }
       pop();
     }
-  })
+  });
 }
 
-class SimpleLines extends Layer {
-  constructor() {
-    super();
-    this.numOfSteps = randomSelectTwo() ? this.stepsOut : int(this.stepsOut * 1.25);
-    this.numOfShapes = randomSelectTwo() ? this.sides : this.sides * 2;
-    this.step = (CRYSTAL_SIZE / 2) / this.numOfSteps; // (radius / number of steps)
-    this.start = floor(random(0, this.numOfSteps));
-    this.stop = floor(random(this.start, this.numOfSteps + 1));
-    this.weight = randomSelectTwo() ? this.thinStroke : this.thickStroke;
-    this.angle = 360 / this.numOfShapes;
-  }
+const simpleLines = (state) => {
+  state.numOfSteps = randomSelectTwo() ? state.stepsOut : int(state.stepsOut * 1.25);
+  state.numOfShapes = randomSelectTwo() ? state.sides : state.sides * 2;
+  state.step = (CRYSTAL_SIZE / 2) / state.numOfSteps; // (radius / number of steps)
+  state.start = floor(random(0, state.numOfSteps));
+  state.stop = floor(random(state.start, state.numOfSteps + 1));
+  state.weight = randomSelectTwo() ? state.thinStroke : state.thickStroke;
+  state.angle = 360 / state.numOfShapes;
 
-  render() {
-    noFill();
-    stroke(this.layerColour);
-    strokeWeight(this.weight);
-    push();
-    // translate(width / 2, height / 2);
-    for (let i = 0; i < this.numOfShapes; i++) {
-      line(this.start * this.step, 0, this.stop * this.step, 0);
-      rotate(this.angle);
-    }
-    pop();
-  }
-}
-
-class OutlineShape extends Layer {
-  constructor() {
-    super();
-    this.weight = randomSelectTwo() ? this.thinStroke : this.thickStroke;
-    this.hexagonTrue = randomSelectTwo();
-  }
-
-  render() {
-    stroke(this.layerColour);
-    strokeWeight(this.weight);
-    push();
-    // translate(width / 2, height / 2);
-    if (this.hexagonTrue) {
-      hexagon(0, 0, CRYSTAL_SIZE / 2);
-    } else {
-      ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-    }
-    pop();
-  }
-}
-
-class DottedLines extends Layer {
-  constructor() {
-    super();
-    this.numOfShapes = randomSelectTwo() ? this.sides : this.sides * 2;
-    this.angle = 360 / this.numOfShapes;
-    this.shapeSize = 3;
-    this.centerOffset = this.singleStep;
-  }
-
-  render() {
-    fill(this.layerColour);
-    noStroke();
-    push();
-    // translate(width / 2, height / 2);
-    for (let i = 0; i <= this.numOfShapes; i++) {
-      for (let x = this.centerOffset; x < CRYSTAL_SIZE / 2; x += this.singleStep) {
-        rect(x, 0, this.shapeSize, this.shapeSize);
+  return ({
+    name: 'Simple Lines',
+    render: () => {
+      noFill();
+      stroke(state.layerColour);
+      strokeWeight(state.weight);
+      push();
+      // translate(width / 2, height / 2);
+      for (let i = 0; i < state.numOfShapes; i++) {
+        line(state.start * state.step, 0, state.stop * state.step, 0);
+        rotate(state.angle);
       }
-      rotate(this.angle);
+      pop();
     }
-    pop();
-  }
+  });
 }
 
-class CenteredShape extends Layer {
-  constructor() {
-    super();
-    this.randomShape = random(1)
-    this.shapeSize = floor(random(this.stepsOut / 2, this.stepsOut - 2)) * this.singleStep;
-  }
+const outlineShape = (state) => {
+  state.weight = randomSelectTwo() ? state.thinStroke : state.thickStroke;
+  state.hexagonTrue = randomSelectTwo();
 
-  render() {
-    fill(this.layerColour);
-    noStroke();
-    push();
-    // translate(width / 2, height / 2);
-    if (this.randomShape < 0.1) {
-      rect(0, 0, this.shapeSize * 2, this.shapeSize * 2);
-    } else if (this.randomShape >= 0.1 && this.randomShape < 0.6) {
-      ellipse(0, 0, this.shapeSize * 2, this.shapeSize * 2);
-    } else if (this.randomShape >= 0.6) {
-      rotate(this.angle / 2);
-      hexagon(0, 0, this.shapeSize);
-    }
-    pop();
-  }
-}
-
-class RingOfShapes extends Layer {
-  constructor() {
-    super();
-    this.steps = floor(random(1, this.stepsOut));
-    this.center = this.steps * this.singleStep;
-    this.randomShape = random(1);
-    this.direction = randomSelectTwo(); // used for triangle only
-    this.fillColour = randomSelectTwo() ? this.layerColour : color(0, 1);
-    this.weight = randomSelectTwo() ? this.thinStroke : this.thickStroke;
-
-    if (this.steps < this.stepsOut / 2) {
-      this.radius = floor(random(1, this.steps)) * this.singleStep;
-    } else if (this.steps > this.stepsOut / 2) {
-      this.radius = floor(random(1, this.stepsOut - this.steps)) * this.singleStep;
-    } else {
-      this.radius = floor(random(1, (this.stepsOut / 2) + 1)) * this.singleStep;
-    }
-  }
-
-  render() {
-    stroke(this.layerColour);
-    fill(this.fillColour);
-    strokeWeight(this.weight);
-    push();
-    // translate(width / 2, height / 2);
-    for (let i = 0; i < this.numOfShapes; i++) {
-      if (this.randomShape < 0.33) {
-        ellipse(0, this.center, this.radius, this.radius);
-      } else if (this.randomShape >= 0.33 && this.randomShape < 0.66) {
-        rect(0, this.center, this.radius, this.radius);
-      } else if (this.randomShape >= 0.66) {
-        myTriangle(this.center, this.radius, this.direction);
+  return ({
+    name: 'Outline Shape',
+    render: () => {
+      stroke(state.layerColour);
+      strokeWeight(state.weight);
+      push();
+      // translate(width / 2, height / 2);
+      if (state.hexagonTrue) {
+        hexagon(0, 0, CRYSTAL_SIZE / 2);
+      } else {
+        ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
       }
-      rotate(this.angle);
+      pop();
     }
-    pop();
-  }
+  });
 }
 
-class SteppedHexagons extends Layer {
-  constructor() {
-    super();
-    this.numOfSteps = randomSelectTwo() ? this.stepsOut : this.stepsOut * 1.25;
-    this.centerOffset = (CRYSTAL_SIZE / 2) * 0.15;
-    this.singleStep = ((CRYSTAL_SIZE / 2) - this.centerOffset) / this.numOfSteps;
-    this.weight = randomSelectTwo() ? this.thinStroke : this.thickStroke;
-    // console.log('numOfSteps', this.numOfSteps);
-    // console.log('centerOffset' ,this.centerOffset);
-    // console.log('singleStep', this.singleStep);
+const dottedLines = (state) => {
+  state.numOfShapes = randomSelectTwo() ? state.sides : state.sides * 2;
+  state.angle = 360 / state.numOfShapes;
+  state.shapeSize = 3;
+  state.centerOffset = state.singleStep;
+
+  return ({
+    name: 'Dotted Lines',
+    render: () => {
+      fill(state.layerColour);
+      noStroke();
+      push();
+      // translate(width / 2, height / 2);
+      for (let i = 0; i <= state.numOfShapes; i++) {
+        for (let x = state.centerOffset; x < CRYSTAL_SIZE / 2; x += state.singleStep) {
+          rect(x, 0, state.shapeSize, state.shapeSize);
+        }
+        rotate(state.angle);
+      }
+      pop();
+    }
+  });
+}
+
+const centeredShape = (state) => {
+  state.randomShape = random(1)
+  state.shapeSize = floor(random(state.stepsOut / 2, state.stepsOut - 2)) * state.singleStep;
+
+  return ({
+    name: 'Centered Shape',
+    render: () => {
+      fill(state.layerColour);
+      noStroke();
+      push();
+      // translate(width / 2, height / 2);
+      if (state.randomShape < 0.1) {
+        rect(0, 0, state.shapeSize * 2, state.shapeSize * 2);
+      } else if (state.randomShape >= 0.1 && state.randomShape < 0.6) {
+        ellipse(0, 0, state.shapeSize * 2, state.shapeSize * 2);
+      } else if (state.randomShape >= 0.6) {
+        rotate(state.angle / 2);
+        hexagon(0, 0, state.shapeSize);
+      }
+      pop();
+    }
+  });
+}
+
+const ringOfShapes = (state) => {
+  state.steps = floor(random(1, state.stepsOut));
+  state.center = state.steps * state.singleStep;
+  state.randomShape = random(1);
+  state.direction = randomSelectTwo(); // used for triangle only
+  state.fillColour = randomSelectTwo() ? state.layerColour : color(0, 1);
+  state.weight = randomSelectTwo() ? state.thinStroke : state.thickStroke;
+
+  if (state.steps < state.stepsOut / 2) {
+    state.radius = floor(random(1, state.steps)) * state.singleStep;
+  } else if (state.steps > state.stepsOut / 2) {
+    state.radius = floor(random(1, state.stepsOut - state.steps)) * state.singleStep;
+  } else {
+    state.radius = floor(random(1, (state.stepsOut / 2) + 1)) * state.singleStep;
   }
 
-  render() {
-    stroke(this.layerColour);
-    noFill();
-    strokeWeight(this.weight);
-    push();
-    // translate(width / 2, height / 2);
-    rotate(this.angle / 2);
-    for (let i = 1; i <= this.numOfSteps; i++) {
-      hexagon(0, 0, this.centerOffset + (i * this.singleStep));
+  return ({
+    name: 'Ring of Shapes',
+    render: () => {
+      stroke(state.layerColour);
+      fill(state.fillColour);
+      strokeWeight(state.weight);
+      push();
+      // translate(width / 2, height / 2);
+      for (let i = 0; i < state.numOfShapes; i++) {
+        if (state.randomShape < 0.33) {
+          ellipse(0, state.center, state.radius, state.radius);
+        } else if (state.randomShape >= 0.33 && state.randomShape < 0.66) {
+          rect(0, state.center, state.radius, state.radius);
+        } else if (state.randomShape >= 0.66) {
+          myTriangle(state.center, state.radius, state.direction);
+        }
+        rotate(state.angle);
+      }
+      pop();
     }
-    pop();
-  }
+  });
+}
+
+const steppedHexagons = (state) => {
+  state.numOfSteps = randomSelectTwo() ? state.stepsOut : state.stepsOut * 1.25;
+  state.centerOffset = (CRYSTAL_SIZE / 2) * 0.15;
+  state.singleStep = ((CRYSTAL_SIZE / 2) - state.centerOffset) / state.numOfSteps;
+  state.weight = randomSelectTwo() ? state.thinStroke : state.thickStroke;
+  // console.log('numOfSteps', state.numOfSteps);
+  // console.log('centerOffset' ,state.centerOffset);
+  // console.log('singleStep', state.singleStep);
+
+  return ({
+    name: 'Stepped Hexagons',
+    render: () => {
+      stroke(state.layerColour);
+      noFill();
+      strokeWeight(state.weight);
+      push();
+      // translate(width / 2, height / 2);
+      rotate(state.angle / 2);
+      for (let i = 1; i <= state.numOfSteps; i++) {
+        hexagon(0, 0, state.centerOffset + (i * state.singleStep));
+      }
+      pop();
+    }
+  });
 }
